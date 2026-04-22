397d2860c684a8ca92a48a21ce730ddc120f6154
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-22 22:03:56 +0530

    Plumbing page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Help from "./pages/Help";
+import Plumbing from "./pages/Plumbing";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/plumbing" element={<Plumbing />} />
       <Route path="/projects" element={<About />} />
       <Route path="/settings" element={<Help />} />
     </Routes>
---
+++ src/pages/Plumbing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const Plumbing = () => {
+  return (
+    <div>
+      <Navbar />
+      plumbing
+      <Footer />
+    </div>
+  );
+};
+
+export default Plumbing;
