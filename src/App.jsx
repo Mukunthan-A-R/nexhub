c6614246c0689d40f4b7cec08d3a7892995f785e
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-23 10:28:24 +0530

    car mechanic

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Help from "./pages/Help";
 import Plumbing from "./pages/Plumbing";
+import CarMechanic from "./pages/CarMechanic";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/plumbing" element={<Plumbing />} />
+      <Route path="/car-mechanic" element={<CarMechanic />} />
       <Route path="/about" element={<About />} />
       <Route path="/help" element={<Help />} />
     </Routes>
