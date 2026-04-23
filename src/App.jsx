b0b5e2d67dba88bab1c949d52fade75462253612
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-23 10:44:18 +0530

    Cleaning service page

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,19 @@ import Plumbing from "./pages/Plumbing";
 import CarMechanic from "./pages/CarMechanic";
 import Electrician from "./pages/Electrician";
 import BikeMechanic from "./pages/BikeMechanic";
+import Cleaner from "./pages/Cleaner";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
       <Route path="/plumbing" element={<Plumbing />} />
       <Route path="/electrician" element={<Electrician />} />
       <Route path="/car-mechanic" element={<CarMechanic />} />
       <Route path="/bike-mechanic" element={<BikeMechanic />} />
+      <Route path="/cleaning-work" element={<Cleaner />} />
+
       <Route path="/about" element={<About />} />
       <Route path="/help" element={<Help />} />
     </Routes>
