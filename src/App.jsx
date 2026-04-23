eb2595acda8313b4ca9828a49cd8e3d7bd3522f5
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-23 10:33:26 +0530

    Electricain work

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import About from "./pages/About";
 import Help from "./pages/Help";
 import Plumbing from "./pages/Plumbing";
 import CarMechanic from "./pages/CarMechanic";
+import Electrician from "./pages/Electrician";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/plumbing" element={<Plumbing />} />
+      <Route path="/electrician" element={<Electrician />} />
       <Route path="/car-mechanic" element={<CarMechanic />} />
       <Route path="/about" element={<About />} />
       <Route path="/help" element={<Help />} />
