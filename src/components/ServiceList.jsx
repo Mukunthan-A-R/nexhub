edb75aaf84ce016dff1665a793ba0c2a82c1975e
Mukunthan A R <mukunthan.1255@gmail.com>
2026-04-23 10:16:58 +0530

    adding search criteria

--- src/components/ServiceList.jsx
+++ src/components/ServiceList.jsx
@@ -1,7 +1,19 @@
-import { Phone, MapPin } from "lucide-react";
+import { useState, useMemo } from "react";
 import ServiceCard from "./ServiceCard";
+import { Search } from "lucide-react";
 
-export default function ServiceList({ title, data }) {
+export default function ServiceList({ title, data = [] }) {
+  const [search, setSearch] = useState("");
+
+  // derived filtered list (case-insensitive)
+  const filteredData = useMemo(() => {
+    if (!search.trim()) return data;
+
+    return data.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()),
+    );
+  }, [search, data]);
+
   return (
     <div className="py-10">
       <div className="max-w-5xl mx-auto px-4 space-y-4">
@@ -10,17 +22,48 @@ export default function ServiceList({ title, data }) {
           {title} Services Available Near You
         </h2>
 
-        {data.map((item) => (
-          <div key={item.phone}>
-            {/* List */}
-            <ServiceCard
-              keyVal={item.phone}
-              name={item.name}
-              role={item.role}
-              phone={item.phone}
-              location={item.location}
+        {/* Search Input */}
+        <div className="w-full md:w-96">
+          <label className="sr-only">Search services</label>
+
+          <div className="relative">
+            {/* Icon */}
+            <Search
+              size={18}
+              className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"
+            />
+
+            {/* Input */}
+            <input
+              type="text"
+              placeholder="Search by name..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              className="
+        w-full pl-10 pr-4 py-2.5
+        text-sm text-gray-800 placeholder-gray-400
+        bg-white
+        border border-gray-300
+        rounded-md
+        shadow-sm
+        focus:outline-none
+        focus:ring-2 focus:ring-purple-600
+        focus:border-purple-600
+        transition
+      "
             />
           </div>
+        </div>
+
+        {/* List */}
+        {filteredData.map((item) => (
+          <ServiceCard
+            key={item.phone}
+            name={item.name}
+            role={item.role}
+            phone={item.phone}
+            location={item.location}
+          />
         ))}
       </div>
     </div>
