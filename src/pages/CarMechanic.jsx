b15fe2dc594d6c6a32e8db330ce3485092c99652
Mukunthan <mukunthan.1255@gmail.com>
2026-05-03 21:36:41 +0530

    adding car mechanic details

--- src/pages/CarMechanic.jsx
+++ src/pages/CarMechanic.jsx
@@ -5,43 +5,61 @@ import ServiceList from "../components/ServiceList";
 
 const CarMechanic = () => {
   const data = [
-    {
-      name: "Raghavendra Rao",
-      role: "Car Mechanic",
-      phone: "+91 9384726150",
-      location: "Ekkatuthangal, Chennai",
-    },
-    {
-      name: "Imran Khan",
-      role: "Car Mechanic",
-      phone: "+91 9021567843",
-      location: "Triplicane, Chennai",
-    },
-    {
-      name: "Lokesh Kumar",
-      role: "Car Mechanic",
-      phone: "+91 9172635480",
-      location: "Koyambedu, Chennai",
-    },
-    {
-      name: "Praveen Shetty",
-      role: "Car Mechanic",
-      phone: "+91 9890345612",
-      location: "Alwarpet, Chennai",
-    },
-    {
-      name: "Faizal Ahmed",
-      role: "Car Mechanic",
-      phone: "+91 9756123489",
-      location: "Teynampet, Chennai",
-    },
-    {
-      name: "Bharath R",
-      role: "Car Mechanic",
-      phone: "+91 9001234786",
-      location: "Kolathur, Chennai",
-    },
-  ];
+  {
+    name: "Chakkarai",
+    role: "Car Mechanic",
+    phone: "9551330333",
+    location: "Ashok Nagar, Chennai",
+  },
+  {
+    name: "Rajan",
+    role: "Car Mechanic",
+    phone: "9444493934",
+    location: "Chennai",
+  },
+  {
+    name: "Muthu",
+    role: "Car Mechanic",
+    phone: "9710691110",
+    location: "Pallavaram, Chennai",
+  },
+  {
+    name: "Antony",
+    role: "Car Mechanic",
+    phone: "6379228288",
+    location: "Chennai",
+  },
+  {
+    name: "Daniel",
+    role: "Car Mechanic",
+    phone: "9444744263",
+    location: "Chennai",
+  },
+  {
+    name: "Dhayaalan",
+    role: "Car Mechanic",
+    phone: "9380905273",
+    location: "Kadapperi, Chennai",
+  },
+  {
+    name: "Sahayam",
+    role: "Car Mechanic",
+    phone: "9791056229",
+    location: "Sembakkam, Chennai",
+  },
+  {
+    name: "Sahayam",
+    role: "Car Mechanic",
+    phone: "7358299722",
+    location: "Sembakkam, Chennai",
+  },
+  {
+    name: "Swamy",
+    role: "Car Mechanic",
+    phone: "7305054684",
+    location: "Sembakkam, Chennai",
+  }
+];
 
   return (
     <div className="min-h-screen flex flex-col bg-[#f8f4fb] ">
